7657edf1fcdaf6bbeabab184dc84d71b28679e07
Erazal75 <lazare.grail@gmail.com>
2025-04-15 10:33:08 +0100

    correction pb dowload files

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ import numpy as np
 import logging
 from contextlib import asynccontextmanager
 import asyncio
+import os
+
+print("Exists:", os.path.exists('/app/JupiterNoteBook/Gaussian Process_model.sav'))
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -15,22 +18,15 @@ model = None
 
 feature_names = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
 
+def load_model():
+    with open("/app/JupiterNoteBook/gaussian_process_model.sav", "rb") as f:
+        return pickle.load(f)
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global model
-    #raise Exception("Sorry, no numbers below zero")
-    # Charger le modèle entraîné
-    #print("azeaeazezaeazze")
-    async def load_model():
-        with open('/app/JupiterNoteBook/Gaussian Process_model.sav', "rb") as f:
-            return pickle.load(f)
     model = await asyncio.to_thread(load_model)
-    # X = np.array([[6.0, 3.3, 3.2, 2.0]])
-    # data = pd.DataFrame(X, columns=feature_names)
-    # logger.info(f"{data}")
-    # prediction = model.predict(data)
-    # logger.info(f"prediction: {str(prediction[0])}")
-    yield 
+    yield
 
 # Initialiser l'application FastAPI
 app = FastAPI(lifespan=lifespan)
